ae558460ca120d13cbde8f1ddcc1954f40f2c0b1
Kisliuk Pavel <39828837+kisliukpavel@users.noreply.github.com>
2018-06-01 20:37:43 +0300

    COMMIT

---
+++ Exercize_2_16Controller.java
@@ -0,0 +1,29 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.InputMethodEvent;
+
+
+
+public class Exercize_2_16Controller {
+
+	@FXML
+	private TextField number1_Text_Field;
+
+	@FXML
+	private Label result_Label;
+
+	@FXML
+	private TextField number2_Text_Field;
+
+	@FXML
+	void number1_Text_Field_Input(InputMethodEvent event) {
+
+	}
+
+	@FXML
+	void number2_Text_Field_Input(InputMethodEvent event) {
+
+	}
+
+}
